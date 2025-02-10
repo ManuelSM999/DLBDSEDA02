d5fcdc6c66f3c706e824587618161e1854dcedb4
manuel <manuel.smetana@iu-study.org>
2025-02-10 21:23:36 +0100

    using a dict for preprocessing including DataFrames from Pandas to visualize the top 10 of most common words

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
 import re
 import csv
+import nltk
+import pandas as pd
+from tabulate import tabulate
 from sklearn.decomposition import TruncatedSVD, LatentDirichletAllocation
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
 from nltk.corpus import stopwords
+from nltk.corpus import words
+
+# download wordl ist from nltk
+nltk.download('words')
 
 
 def print_topics(model, words_per_topic):
@@ -22,7 +29,7 @@ def print_topics(model, words_per_topic):
 
 
 reviews = []
-stop_words = stopwords.words('english')
+word_list = set(words.words())
 
 with open('Musical_instruments_reviews.csv', mode='r', newline='', encoding='utf-8') as file:
     content = csv.DictReader(file)
@@ -31,8 +38,12 @@ with open('Musical_instruments_reviews.csv', mode='r', newline='', encoding='utf
         # only letters and spaces to tokenize the words
         clean_review_text = re.sub(r'[^a-zA-Z\s]', '',
                                    dirty_review_text)
-        reviews.append(clean_review_text)
+        tokenize_words = clean_review_text.split()
+        # check if a word is included in the downloaded dictionary (word_list)
+        valid_words = [word for word in tokenize_words if word in word_list]
+        reviews.append(" ".join(valid_words))
 
+stop_words = stopwords.words('english')
 # vectorizers assign a value to all words in all reviews
 # init vectorizers with Parameters (stopwords)
 tfidf_vectorizer = TfidfVectorizer(stop_words=stop_words)
@@ -40,6 +51,20 @@ count_vectorizer = CountVectorizer(stop_words=stop_words)
 # add data to vectorizers
 tfidf_data = tfidf_vectorizer.fit_transform(reviews)
 count_data = count_vectorizer.fit_transform(reviews)
+
+# convert vectors to DataFrames
+tfidf_data_frame = pd.DataFrame(tfidf_data.toarray(), columns=tfidf_vectorizer.get_feature_names_out())
+count_data_frame = pd.DataFrame(count_data.toarray(), columns=count_vectorizer.get_feature_names_out())
+# sum of all words from the reviews; ascending=False to get most frequent words
+tfidf_sums = tfidf_data_frame.sum().sort_values(ascending=False)
+count_sums = count_data_frame.sum().sort_values(ascending=False)
+
+# to pretty print the data with Pandas
+print('-----------------Top 10 TF-IDF_Vector-----------------')
+print(tabulate(tfidf_sums.head(10).reset_index(), tablefmt='pretty', headers=['Word', 'Value']))
+print('-----------------Top 10 Count_Vector-----------------')
+print(tabulate(count_sums.head(10).reset_index(), tablefmt='pretty', headers=['Word', 'Value']))
+print()
 
 number_of_topics = 5
 number_of_words_per_topic = 10
